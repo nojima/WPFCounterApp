78168b4c90baf98100a4eb12af732146cf0855dc
Yusuke Nojima <nojima@ynojima.com>
2020-08-15 16:38:03 +0900

    CounterAppをナイーブに実装

--- CounterApp/MainWindow.xaml.cs
+++ CounterApp/MainWindow.xaml.cs
@@ -1,17 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace CounterApp
 {
@@ -20,9 +7,23 @@ namespace CounterApp
     /// </summary>
     public partial class MainWindow : Window
     {
+        private int count = 0;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void btnIncrement_Click(object sender, RoutedEventArgs e)
+        {
+            count++;
+            tbCounter.Text = count.ToString();
+        }
+
+        private void btnDecrement_Click(object sender, RoutedEventArgs e)
+        {
+            count--;
+            tbCounter.Text = count.ToString();
         }
     }
 }
