76e4dc7a01b0b0d084643b959ef3d637722928c4
Yusuke Nojima <nojima@ynojima.com>
2020-08-15 16:47:18 +0900

    要件を追加

--- CounterApp/MainWindow.xaml.cs
+++ CounterApp/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows;
+using System.Windows.Media;
 
 namespace CounterApp
 {
@@ -7,23 +8,41 @@ namespace CounterApp
     /// </summary>
     public partial class MainWindow : Window
     {
+        private const int MAX_COUNT = 10;
+        private const int MIN_COUNT = -10;
+
         private int count = 0;
 
         public MainWindow()
         {
             InitializeComponent();
+            UpdateView();
         }
 
         private void btnIncrement_Click(object sender, RoutedEventArgs e)
         {
-            count++;
-            tbCounter.Text = count.ToString();
+            if (count < MAX_COUNT)
+                count++;
+            UpdateView();
         }
 
         private void btnDecrement_Click(object sender, RoutedEventArgs e)
         {
-            count--;
-            tbCounter.Text = count.ToString();
+            if (count > MIN_COUNT)
+                count--;
+            UpdateView();
+        }
+
+        private void UpdateView()
+        {
+            tbCounter.Text = count.ToString("+#;-#;0"); // +符号を表示する
+
+            if (count < 0)
+                tbCounter.Foreground = Brushes.Red;
+            else if (count > 0)
+                tbCounter.Foreground = Brushes.Green;
+            else
+                tbCounter.Foreground = Brushes.Gray;
         }
     }
 }
